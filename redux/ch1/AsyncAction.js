4fc94ac60e979dcf41b13876828e9ac1194da401
Krishna Jain <120629145+krishnajain872@users.noreply.github.com>
2023-03-21 10:08:16 +0530

    Working with thunk

--- redux/ch1/AsyncAction.js
+++ redux/ch1/AsyncAction.js
@@ -1,14 +1,16 @@
 // console.log("hello world")
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger"
+import thunk from "redux-thunk"
 
 // Action name constants
 const INC = "INCREMENT"
 const DEC = "DECREMENT"
 
+// store
+const store = createStore(reducer, applyMiddleware(logger.default, thunk.default))
 
-const store = createStore(reducer, applyMiddleware(logger.default))
-
+// reducer funtion
 function reducer(state = { amount: 0 }, action) {
     if (action.type == "INCREMENT") {
         // console.log("state should be performed with some action ")
@@ -22,18 +24,6 @@ function reducer(state = { amount: 0 }, action) {
 }
 
 
-// action
-// {type:INCREMENT}  action is a object which have a type and payload although action can consist of other values too according to the use case
-
-// how to send action to the redux store so for that we use dispatch
-// store.dispatch({ type: "INCREMENT", payload: 1 })
-
-
-// global state 
-// store.subscribe(() => { //when ever a reducer funtion executes this subscribe funtion from store will run automatically
-//     history.push(store.getState())
-//     console.log(store.getState())
-// })
 
 // create ACTION funtions
 
@@ -48,8 +38,7 @@ function Decrement(value) {
 
 const history = []
 setInterval(() => {
-    // store.dispatch({ type: "INCREMENT", payload: 100 })
-    // store.dispatch({ type: "DECREMENT", payload: 100 })
+
     store.dispatch(Increment(20))
 }, 4000)
-// setTimeout(() => { console.log(history) }, 10000)
+
